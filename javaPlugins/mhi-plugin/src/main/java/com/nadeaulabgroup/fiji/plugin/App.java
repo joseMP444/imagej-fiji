5ecde6a3aa2493055314e1606094eec8a313b02b
evaunit444 <josemp0259@gmail.com>
2023-11-30 00:19:39 -0800

    MHI Plugin

---
+++ javaPlugins/mhi-plugin/src/main/java/com/nadeaulabgroup/fiji/plugin/App.java
@@ -0,0 +1,13 @@
+package com.nadeaulabgroup.fiji.plugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
